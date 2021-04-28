ef436c55803b5379264dc433261b3d02974b1c6e
pavelkalinchuk <715m4s@gmail.com>
2021-04-28 17:36:23 +0300

    Update code

--- tests/test_first.py
+++ tests/test_first.py
@@ -1,7 +1,7 @@
-# -*- coding: utf-8 -*-
+ -*- coding: utf-8 -*-
 from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.support.ui import Select
+# from selenium.webwd.common.keys import Keys
+# from selenium.webwd.support.ui import Select
 # from selenium.common.exceptions import NoSuchElementException
 # from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
@@ -10,38 +10,38 @@ import pytest
 
 class Test(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path='/Users/pavelkalincuk/git/help_soft/chromedriver')
-        self.driver.implicitly_wait(30)
+        self.wd = webwd.Chrome(executable_path='/Users/pavelkalincuk/git/help_soft/chromedriver')
+        self.wd.implicitly_wait(30)
 
     def test_(self):
-        driver = self.driver
-        driver.get("http://localhost/addressbook/index.php")
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_xpath("//input[@value='Login']").click()
-        driver.find_element_by_link_text("Logout").click()
-        driver.close()
+        driver = self.wd
+        wd.get("http://localhost/addressbook/index.php")
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+        wd.find_element_by_link_text("Logout").click()
+        wd.close()
 
 #    def is_element_present(self, how, what):
 #        try:
-#            self.driver.find_element(by=how, value=what)
+#            self.wd.find_element(by=how, value=what)
 #        except NoSuchElementException as e:
 #            return False
 #        return True
 #
 #    def is_alert_present(self):
 #        try:
-#            self.driver.switch_to_alert()
+#            self.wd.switch_to_alert()
 #        except NoAlertPresentException as e:
 #            return False
 #        return True
 #
 #    def close_alert_and_get_its_text(self):
 #        try:
-#            alert = self.driver.switch_to_alert()
+#            alert = self.wd.switch_to_alert()
 #            alert_text = alert.text
 #            if self.accept_next_alert:
 #                alert.accept()
@@ -52,7 +52,7 @@ class Test(unittest.TestCase):
 #            self.accept_next_alert = True
 
     def tearDown(self):
-        self.driver.quit()
+        self.wd.quit()
 
 
 if __name__ == "__main__":
