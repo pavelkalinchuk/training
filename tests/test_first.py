5d6be1f63f8b4b8b38b38e4cd32e3c9d0680a6a6
pavel.kalinchuk <715m4s@gmail.com>
2021-04-28 16:20:53 +0300

    New commit

--- tests/test_first.py
+++ tests/test_first.py
@@ -1,20 +1,18 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.support.ui import Select
+# from selenium.common.exceptions import NoSuchElementException
+# from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
+import pytest
 
 
 class Test(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(executable_path='/Users/pavelkalincuk/git/help_soft/chromedriver')
         self.driver.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
 
     def test_(self):
         driver = self.driver
@@ -28,35 +26,34 @@ class Test(unittest.TestCase):
         driver.find_element_by_link_text("Logout").click()
         driver.close()
 
-    def is_element_present(self, how, what):
-        try:
-            self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.driver.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally:
-            self.accept_next_alert = True
+#    def is_element_present(self, how, what):
+#        try:
+#            self.driver.find_element(by=how, value=what)
+#        except NoSuchElementException as e:
+#            return False
+#        return True
+#
+#    def is_alert_present(self):
+#        try:
+#            self.driver.switch_to_alert()
+#        except NoAlertPresentException as e:
+#            return False
+#        return True
+#
+#    def close_alert_and_get_its_text(self):
+#        try:
+#            alert = self.driver.switch_to_alert()
+#            alert_text = alert.text
+#            if self.accept_next_alert:
+#                alert.accept()
+#            else:
+#                alert.dismiss()
+#            return alert_text
+#        finally:
+#            self.accept_next_alert = True
 
     def tearDown(self):
         self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
 
 
 if __name__ == "__main__":
