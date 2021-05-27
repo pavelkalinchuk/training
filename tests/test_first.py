f45af902a6337d840a4cff9cd7180e8dece260d2
pavelkalinchuk <715m4s@gmail.com>
2021-05-27 13:33:24 +0300

    Update code

--- tests/test_first.py
+++ tests/test_first.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import time
+
 from selenium import webdriver
 import unittest
 
@@ -17,6 +19,20 @@ class Test(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
+        wd.find_element_by_link_text("groups").click()
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("test_group")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("test")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("test")
+        wd.find_element_by_name("submit").click()
+        wd.find_element_by_link_text("groups").click()
+        time.sleep(3)
         wd.find_element_by_link_text("Logout").click()
         wd.close()
 
